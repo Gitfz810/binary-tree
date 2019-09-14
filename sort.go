cd1703514eb5f96f9ccbdbfbfab25476c71ea2f6
Gitfz810 <mailfz810@163.com>
2019-09-14 10:56:17 +0800

    summarize sort funcation

--- sort.go
+++ sort.go
@@ -5,15 +5,82 @@ import "fmt"
 func shellSort(s []int) {
 	increment, length := 1, len(s)
 	for increment < length / 3 {
-		increment = 3 * increment + 1
+		increment = increment * 3 + 1
 	}
+
 	for increment >= 1 {
 		for i := increment; i < length; i++ {
-			for j := i; j >= increment && s[j] < s[j-increment]; j -= increment {
-				s[j], s[j-increment] = s[j-increment], s[j]
+			for j := i; j >= increment; j -= increment {
+				if s[j] < s[j-increment] {
+					s[j], s[j-increment] = s[j-increment], s[j]
+				}
 			}
 		}
 		increment /= 3
+	}
+}
+
+func heapSort(s []int) {
+	length := len(s)
+	for i := length/2-1; i >= 0; i-- {
+		heapAdjust(s, i, length)
+	}
+
+	for i := length-1; i > 0; i-- {
+		s[0], s[i] = s[i], s[0]
+		heapAdjust(s, 0, i)
+	}
+}
+
+func heapAdjust(s []int, pos int, length int) {
+	for i := 2*pos+1; i <= length-1; i = 2*pos+1 {
+		if i < length-1 && s[i] < s[i+1] {
+			i++
+		}
+
+		if s[pos] > s[i] {
+			break
+		}
+
+		s[pos], s[i] = s[i], s[pos]
+		pos = i
+	}
+}
+
+func mergeSort(s []int, low, high int) {
+	if low >= high {
+		return
+	}
+
+	mid := (high + low) / 2
+	mergeSort(s, low, mid)
+	mergeSort(s, mid+1, high)
+	merge(s, low, mid, high)
+}
+
+func merge(s []int, low int, mid int, high int) {
+	temp := make([]int, high-low+1)
+	for i := low; i <= high; i++ {
+		temp[i-low] = s[i]
+	}
+
+	left := low
+	right := mid + 1
+
+	for i := low; i <= high; i++ {
+		if right > high {
+			s[i] = temp[left-low]
+			left++
+		} else if left > mid {
+			s[i] = temp[right-low]
+			right++
+		} else if temp[left-low] < temp[right-low] {
+			s[i] = temp[left-low]
+			left++
+		} else {
+			s[i] = temp[right-low]
+			right++
+		}
 	}
 }
 
@@ -21,7 +88,7 @@ func quickSort(s []int) {
 	qSort(s, 0, len(s)-1)
 }
 
-func qSort(s []int, low, high int) {
+func qSort(s []int, low int, high int) {
 	if low < high {
 		pivot := partition(s, low, high)
 		qSort(s, pivot+1, high)
@@ -29,38 +96,39 @@ func qSort(s []int, low, high int) {
 	}
 }
 
-func partition(s []int, low, high int) int {
-	// 三数取中，保证pivotKey是数列中的中间值
-	mid := low + (high - low) / 2
-	if s[low] > s[high] {
-		s[low], s[high] = s[high], s[low]
-	}
+func partition(s []int, low int, high int) int {
+	mid := low + (high-low) / 2
 	if s[mid] > s[high] {
 		s[mid], s[high] = s[high], s[mid]
+	}
+	if s[low] > s[high] {
+		s[low], s[high] = s[high], s[low]
 	}
 	if s[mid] > s[low] {
 		s[mid], s[low] = s[low], s[mid]
 	}
 	pivotKey := s[low]
+
 	for low < high {
-		for low < high && s[high] >= pivotKey {
-			high -= 1
+		for low < high && s[high] > pivotKey {
+			high--
 		}
-		// 当右侧值小于pivotKey时，左侧的low索引替换成high索引的小值
 		s[low] = s[high]
-		for low < high && s[low] <= pivotKey {
-			low += 1
+		for low < high && s[low] < pivotKey {
+			low++
 		}
-		// 当左侧值大于pivotKey时，右侧的high索引替换成low索引的大值
 		s[high] = s[low]
 	}
 	s[low] = pivotKey
 	return low
 }
 
-func main() {
-	s := []int{9, 1, 5, 8, 3, 7, 4, 6, 2}
-	shellSort(s)
-	//quickSort(s)
+func main()  {
+	s := []int{19, 27, 6, 32, 12, 22, 26, 17, 23}
+	fmt.Println(s)
+	//shellSort(s)
+	//heapSort(s)
+	//mergeSort(s, 0, len(s)-1)
+	quickSort(s)
 	fmt.Println(s)
 }
